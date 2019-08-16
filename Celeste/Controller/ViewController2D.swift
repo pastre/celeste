616da5240c2bc5ee4bc757bf66e25d3d36d279b0
Filipe Souza <filipesouza005@gmail.com>
2019-08-16 16:49:05 -0300

    Criada

---
+++ Celeste/Controller/ViewController2D.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController2D.swift
+//  Celeste
+//
+//  Created by Filipe Souza on 16/08/19.
+//  Copyright © 2019 Bruno Pastre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2D: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
