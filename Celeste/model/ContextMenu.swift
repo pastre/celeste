fc85c8220f3571ba5e9b09ba06a294d71db0d191
pastre <pastre68@gmail.com>
2019-08-16 18:20:36 -0300

    Created basic ContextMenu classes

---
+++ Celeste/model/ContextMenu.swift
@@ -0,0 +1,21 @@
+//
+//  ContextMenu.swift
+//  Celeste
+//
+//  Created by Bruno Pastre on 16/08/19.
+//  Copyright © 2019 Bruno Pastre. All rights reserved.
+//
+
+import Foundation
+
+enum ContextMenuOption:String, CaseIterable{
+    case createPlanet = "Create Planet"
+}
+
+protocol ContextMenuDelegate {
+    func onOption(option: ContextMenuOption)
+}
+
+class ContextMenu{
+    
+}
