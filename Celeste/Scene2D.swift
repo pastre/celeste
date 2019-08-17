b95fb6ebb947c94e69d4ce9956c236b3286fbfe7
Filipe <filipesouza005@gmail.com>
2019-08-17 19:32:21 -0300

    Adição de ViewController e SKScene pro 2D

---
+++ Celeste/Scene2D.swift
@@ -0,0 +1,14 @@
+//
+//  Scene2D.swift
+//  Celeste
+//
+//  Created by Filipe Souza on 17/08/19.
+//  Copyright © 2019 Bruno Pastre. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class Scene2D: SKScene {
+
+}
